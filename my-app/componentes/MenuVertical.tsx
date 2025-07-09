e4ef44a5828c7fb96d161f6f4891a4d58186c149
josiel <josielsoares_2@hotmail.com>
2025-07-09 18:43:43 -0400

    update: Criado menu lateral esquerdo

--- my-app/componentes/MenuVertical.tsx
+++ my-app/componentes/MenuVertical.tsx
@@ -1,7 +1,21 @@
 export default function MenuVertical(){
   return (
-    <div>
-    
+    <div className="flex space-x-4">
+        <header className= "text-white bg-blue-400 rounded-t-sm w-35 h-screen">
+        <div className=" grid justify-items-start">
+          <div className="p-3 hover:text-gray-400 font-bold">Menu</div>
+          <div className="p-3 hover:text-gray-400">Início</div>
+          <div className="p-3 hover:text-gray-400">Perfil</div>
+          <div className="p-3 hover:text-gray-400">Configurações</div>
+          <div className="p-3 hover:text-gray-400">Sair</div>
+        </div>
+      </header>
+
+       <div className="p-3 text-black">
+        <div className="font-bold pb-2">Bem-vindo!</div>
+        <p className="text-xs">Esta é uma tela com um menu simples usando Tailwind CSS.</p>
+       </div>
+
     </div>
   );
 }
