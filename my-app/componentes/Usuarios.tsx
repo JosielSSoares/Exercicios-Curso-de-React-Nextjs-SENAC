a3c1b6e93dbc7c04b1a9c84ee1a407471c27a699
Senac Hub - Sala 306 <hubsala306@aluno.ms.senac.br>
2025-07-12 16:53:42 -0400

    update: corrigido largura da tela

--- my-app/componentes/Usuarios.tsx
+++ my-app/componentes/Usuarios.tsx
@@ -9,7 +9,7 @@ export default function Usuarios({ titulo, email, botao, funcao }: props) {
     return (
       <div className="p-3">
         <div className="bg-white flex-1 p-5 rounded-lg shadow-xl text-start">
-          <h1 className="text-2xl font-bold text-gray-800 mb-4">{titulo}</h1>
+          <h1 className="text-1 font-bold text-gray-800 mb-4">{titulo}</h1>
           <div className="bg-gray-100 w-35 rounded-md text-gray-700 mb-3 text-left">{email}</div>
           <button className="w-35 py-1 px-6 bg-blue-500 text-white font-semibold rounded-md shadow-md" onClick={funcao}>{botao}
           </button>
